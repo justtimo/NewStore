e12c081b2289035481eb9016bd1d73b2cae39b82
justtimo <wby6225104@163.com>
2020-07-02 21:08:39 +0800

    spu保存

--- store-passport-web/src/main/java/com/wby/store/storepassportweb/controller/PassPortController.java
+++ store-passport-web/src/main/java/com/wby/store/storepassportweb/controller/PassPortController.java
@@ -1,13 +1,85 @@
 package com.wby.store.storepassportweb.controller;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.wby.store.bean.UserInfo;
+import com.wby.store.service.UserService;
+import com.wby.util.JWTUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class PassPortController {
+    @Reference
+    UserService userService;
+
+    String jwtKey="wby";
 
     @GetMapping("index.html")
     public String idnex(){
         return "index";
     }
+
+    /**
+     * 用户登录
+     * @param userInfo
+     * @param request
+     * @return
+     */
+    @PostMapping("login")//查看页面下面的JS方法，
+    @ResponseBody
+    public String login(UserInfo userInfo, HttpServletRequest request){
+
+        UserInfo userInfoExist=userService.login(userInfo);
+        if (userInfoExist!=null){
+            //制作token
+            HashMap<String, Object> map = new HashMap<>();
+            map.put("userId",userInfoExist.getId());
+            map.put("nickName",userInfoExist.getNickName());
+
+            /**
+             * 获取ip:
+             *  1 request.getRemoteAddr()//如果用户直接访问tomcat是可以获取到ip的，但是
+             *      实际上中间经过了Nginx反向代理，取到的就是Nginx的地址，所以要配置Nginx
+             */
+            String remoteAddr = request.getRemoteAddr();
+            String ipAddr = request.getHeader("X-forwarded-for");
+            String token = JWTUtil.encode(jwtKey, map, remoteAddr);
+            return token;
+        }
+        return "fail";
+    }
+
+    /**
+     * 认证服务
+     * @param token
+     * @param currentIp
+     * @return
+     */
+    @GetMapping("verify")
+    public String verify(@RequestParam("token") String token,
+                         @RequestParam("currentIp")String currentIp){
+        //1.验证token
+        Map<String, Object> userMap = JWTUtil.decode(token, "wby", currentIp);
+        //2.验证缓存
+        if (userMap!=null){
+            //2.验证缓存
+            String userId = (String)userMap.get("userId");
+            UserInfo userInfo=userService.verify(userId);
+            if (userInfo!=null){
+                return "success";
+            }
+        }
+
+        return "false";
+
+    }
+
+
 }
--- store-passport-web/src/test/java/com/wby/store/storepassportweb/StorePassportWebApplicationTests.java
+++ store-passport-web/src/test/java/com/wby/store/storepassportweb/StorePassportWebApplicationTests.java
@@ -1,14 +1,38 @@
 package com.wby.store.storepassportweb;
 
 
+import com.alibaba.fastjson.JSON;
+import com.wby.util.JWTUtil;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootTest
-class StorePassportWebApplicationTests {
+@RunWith(SpringRunner.class)
+public class StorePassportWebApplicationTests {
+
+    //JWTUtil jwtUtil;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+    }
+
+    @Test
+    public void test1(){
+        String key = "atguigu";
+        String ip="192.168.67.201";
+        Map map = new HashMap();
+        map.put("userId","1001");
+        map.put("nickName","marry");
+        String token = JWTUtil.encode(key, map, ip);
+        System.out.println("token:::"+token);
+        Map<String, Object> decode = JWTUtil.decode(token, key, "192.168.67.201");
+        String s = JSON.toJSONString(decode);
+        System.out.println("decode:::"+decode);
     }
 
 }
